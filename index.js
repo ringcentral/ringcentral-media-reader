9cc2deebd39bf97a97452a13ca8d589b8a81906d
Embbnux Ji <embbnux@embbnux.com>
2021-12-23 17:26:24 +0800

    use RC JS SDK v4 and authorize with auth code with PKCE

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var environments = {
     attServer: 'https://platform.devtest.ringcentral.com',
     media: 'https://media.devtest.ringcentral.com/',
     attMedia: 'https://media.devtest.ringcentral.com/',
-    appKey: 'pBZD_5h6QCiC3p0NwQcgdQ',
+    clientId: 'pBZD_5h6QCiC3p0NwQcgdQ',
   },
   production: {
     server: 'https://platform.ringcentral.com',
     attServer: 'https://platform.ringcentral.biz',
     media: 'https://media.ringcentral.com/',
     attMedia: 'https://media.ringcentral.biz/',
-    appKey: 'OJ9VDO5STqObDdkolDJRsw',
+    clientId: 'OJ9VDO5STqObDdkolDJRsw',
   }
 };
 
@@ -19,7 +19,7 @@ var brand = 'rc';
 var lastMediaKey = 'lastMedia-' + environment;
 var isAtt = window.location.href.indexOf('.biz') > 0;
 var lastMedia = localStorage.getItem(lastMediaKey);
-if (!isAtt && window.location.href.indexOf('access_token') > 0) {
+if (!isAtt && window.location.href.indexOf('code') > 0) {
   isAtt = lastMedia.indexOf('.biz') > 0;
 }
 if (isAtt) {
@@ -28,14 +28,14 @@ if (isAtt) {
 var rcsdk = new RingCentral.SDK({
   cachePrefix: 'simple-media-player-' + environment + brand,
   server: isAtt ? environments[environment].attServer : environments[environment].server,
-  appKey: environments[environment].appKey,
+  clientId: environments[environment].clientId,
   redirectUri: window.location.protocol + '//' + window.location.host + window.location.pathname,
 });
 var platform = rcsdk.platform();
 
 var urlParams = rcsdk.platform().parseLoginRedirect(window.location.hash || window.location.search || '?a');
 var media = urlParams['media'];
-var token = urlParams['access_token'];
+var authCode = urlParams['code'];
 if (media) {
   media = decodeURIComponent(media);
 }
@@ -54,7 +54,9 @@ platform.loggedIn().then(function(isLogin) {
   console.log('Login Status: ', isLogin);
   if (isLogin && media) {
     if (isValidMediaLink(media)) {
-      window.location.assign(media + '?access_token=' + platform.auth().data().access_token);
+      platform.auth().data().then(function(authData) {
+        window.location.assign(media + '?access_token=' + authData.access_token);
+      });
     }
     return;
   }
@@ -62,22 +64,28 @@ platform.loggedIn().then(function(isLogin) {
   if (!isLogin && media) {
     if (isValidMediaLink(media)) {
       localStorage.setItem(lastMediaKey, media);
-      var loginUrl = platform.loginUrl({implicit: true});
+      var loginUrl = platform.loginUrl({usePKCE: true});
+      var codeVerifier = platform.codeVerifier;
+      localStorage.setItem('simple-media-player-code-verifier', codeVerifier);
       window.location.assign(loginUrl);
     }
     return;
   }
 
-  if (!isLogin && token) {
+  if (!isLogin && authCode) {
+    var codeVerifier = localStorage.getItem('simple-media-player-code-verifier');
+    urlParams.code_verifier = codeVerifier;
     platform.login(urlParams).then(function() {
       lastMedia = localStorage.getItem(lastMediaKey);
       localStorage.removeItem(lastMediaKey);
       if (lastMedia && isValidMediaLink(lastMedia)) {
-        window.location.assign(lastMedia + '?access_token=' + platform.auth().data().access_token);
+        platform.auth().data().then(function(authData) {
+          window.location.assign(lastMedia + '?access_token=' + authData.access_token);
+        });
       }
     });
     return;
   }
   var loading = document.getElementById('loading');
-  loading.innerHTML = 'Nofound.';
+  loading.innerHTML = 'Notfound.';
 });
