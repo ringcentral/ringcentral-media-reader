e3c9fc5a2010432bef4f264463c2b763d18ab9ba
Embbnux Ji <embbnux@embbnux.com>
2021-12-27 16:44:51 +0800

    feat: add archiver storages link

--- index.js
+++ index.js
@@ -50,28 +50,17 @@ function isValidMediaLink(link) {
   return false;
 }
 
+function getAttachmentId(mediaLink) {
+  let purgeMedia = mediaLink.split('?')[0];
+  purgeMedia = purgeMedia.split('#')[0];
+  purgeMedia = purgeMedia.replace(/\/content$/, '');
+  return purgeMedia.split('/').pop();
+}
+
+var logoutButton = window.document.getElementById('logoutButton');
+
 platform.loggedIn().then(function(isLogin) {
   console.log('Login Status: ', isLogin);
-  if (isLogin && media) {
-    if (isValidMediaLink(media)) {
-      platform.auth().data().then(function(authData) {
-        window.location.assign(media + '?access_token=' + authData.access_token);
-      });
-    }
-    return;
-  }
-
-  if (!isLogin && media) {
-    if (isValidMediaLink(media)) {
-      localStorage.setItem(lastMediaKey, media);
-      var loginUrl = platform.loginUrl({usePKCE: true});
-      var codeVerifier = platform.codeVerifier;
-      localStorage.setItem('simple-media-player-code-verifier', codeVerifier);
-      window.location.assign(loginUrl);
-    }
-    return;
-  }
-
   if (!isLogin && authCode) {
     var codeVerifier = localStorage.getItem('simple-media-player-code-verifier');
     urlParams.code_verifier = codeVerifier;
@@ -86,6 +75,74 @@ platform.loggedIn().then(function(isLogin) {
     });
     return;
   }
-  var loading = document.getElementById('loading');
-  loading.innerHTML = 'Notfound.';
+  if (isLogin) {
+    logoutButton.style.display = 'block';
+  }
 });
+
+logoutButton.addEventListener('click', function () {
+  platform.logout().then(function () {
+    window.location.reload();
+  });
+});
+
+var readFromRCServerButton = window.document.getElementById('readFromRCServer');
+readFromRCServerButton.addEventListener('click', function () {
+  if (!isValidMediaLink(media)) {
+    alert('media link is not valid');
+    return;
+  }
+  platform.loggedIn().then(function(isLogin) {
+    if (isLogin && media) {
+      platform.auth().data().then(function(authData) {
+        window.location.assign(media + '?access_token=' + authData.access_token);
+      });
+      return;
+    }
+    if (!isLogin && media) {
+      localStorage.setItem(lastMediaKey, media);
+      var loginUrl = platform.loginUrl({usePKCE: true});
+      var codeVerifier = platform.codeVerifier;
+      localStorage.setItem('simple-media-player-code-verifier', codeVerifier);
+      window.location.assign(loginUrl);
+      return;
+    }
+  });
+});
+
+var readFromGoogleButton = window.document.getElementById('readFromGoogle');
+readFromGoogleButton.addEventListener('click', function () {
+  window.location.assign(`https://drive.google.com/drive/search?q=` + getAttachmentId(media));
+});
+
+var readFromDropboxButton = window.document.getElementById('readFromDropbox');
+readFromDropboxButton.addEventListener('click', function () {
+  window.location.assign(`https://www.dropbox.com/search/personal?path=%2F&query=` + getAttachmentId(media));
+});
+
+var readFromBoxButton = window.document.getElementById('readFromBox');
+readFromBoxButton.addEventListener('click', function () {
+  window.location.assign(`https://app.box.com/folder/0/search?query=` + getAttachmentId(media));
+});
+
+var startGetMediaButton = window.document.getElementById('startGetMedia');
+startGetMediaButton.addEventListener('click', function () {
+  var mediaUri = window.document.getElementById('mediaUri').value;
+  if (!isValidMediaLink(mediaUri)) {
+    alert('media link is not valid');
+    return;
+  }
+  var pageUri = window.location.protocol + '//' + window.location.host + window.location.pathname;
+  window.location.assign(pageUri + '?media=' + encodeURIComponent(mediaUri));
+});
+
+var loading = document.getElementById('loading');
+loading.style.display = 'none';
+var mainPage = document.getElementById('mainPage');
+var startPage = document.getElementById('startPage');
+
+if (!media && !authCode) {
+  startPage.style.display = 'block';
+} else {
+  mainPage.style.display = 'block';
+}
